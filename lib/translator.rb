7156ed39f601f9f70cb7681c5335fae848950c24
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-08 17:08:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_english_meaning(path, emoticon)
     end
   end
   return "Sorry, that emoticon was not found"
+end
   
   def get_japanese_emoticon(path, emoticon)
     load_library(path).each do |key, value|
